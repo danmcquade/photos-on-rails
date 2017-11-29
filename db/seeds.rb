06b81827939133454bc9052d6d19ec64bb72437a
Dan McQuade <dtmcquade@gmail.com>
2017-11-29 15:19:40 -0500

    Update styles and layouts

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,17 @@ User.destroy_all
 Photo.destroy_all
 
 dtm = User.create(email: "dtmcquade@gmail.com", username: "danmcquade", password: "password")
-dan = User.create(email: "dan@danmcq.com", username: "dannyboy", password: "password")
-inf = User.create(email: "info@danmcq.com", username: "infouser", password: "password")
+mike = User.create(email: "mikesmith@gmail.com", username: "mikesmith", password: "password")
+john = User.create(email: "johnjones@gmail.com", username: "johnjones", password: "password")
 
 dtm.photos.create(title: "NYC Highline", url: File.open(File.join(Rails.root, '/app/assets/images/1.jpg')))
 dtm.photos.create(title: "Winter Beach", url: File.open(File.join(Rails.root, '/app/assets/images/2.jpg')))
 dtm.photos.create(title: "Lahinch Coast", url: File.open(File.join(Rails.root, '/app/assets/images/3.jpg')))
 
-dan.photos.create(title: "Irish Cliffs", url: File.open(File.join(Rails.root, '/app/assets/images/4.jpg')))
-dan.photos.create(title: "Maine Ave", url: File.open(File.join(Rails.root, '/app/assets/images/5.jpg')))
-dan.photos.create(title: "Dublin Bridge", url: File.open(File.join(Rails.root, '/app/assets/images/6.jpg')))
+mike.photos.create(title: "Irish Cliffs", url: File.open(File.join(Rails.root, '/app/assets/images/4.jpg')))
+mike.photos.create(title: "Maine Ave", url: File.open(File.join(Rails.root, '/app/assets/images/5.jpg')))
+mike.photos.create(title: "Dublin Bridge", url: File.open(File.join(Rails.root, '/app/assets/images/6.jpg')))
 
-inf.photos.create(title: "Rehoboth Birds", url: File.open(File.join(Rails.root, '/app/assets/images/7.jpg')))
-inf.photos.create(title: "Washington Monument", url: File.open(File.join(Rails.root, '/app/assets/images/8.jpg')))
-inf.photos.create(title: "Monument at Night", url: File.open(File.join(Rails.root, '/app/assets/images/9.jpg')))
+john.photos.create(title: "Rehoboth Birds", url: File.open(File.join(Rails.root, '/app/assets/images/7.jpg')))
+john.photos.create(title: "Washington Monument", url: File.open(File.join(Rails.root, '/app/assets/images/8.jpg')))
+john.photos.create(title: "Monument at Night", url: File.open(File.join(Rails.root, '/app/assets/images/9.jpg')))
