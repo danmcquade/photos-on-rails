c2be84e1ddd25df42b24a2250a13ab99ccb9d32b
Dan McQuade <dtmcquade@gmail.com>
2017-11-30 09:54:39 -0500

    Add comment edit feature, update styles and layouts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,13 +11,28 @@ class CommentsController < ApplicationController
       @comment = @photo.comments.create(comment_params.merge(user: current_user))
       flash[:notice] = "Comment successfully posted."
     end
-    redirect_to user_photo_path(current_user, @photo)
+    redirect_to request.referrer
   end
 
   def edit
+    @user = User.find(params[:user_id])
+    @photo = Photo.find(params[:photo_id])
+    @comment = Comment.find(params[:id])
+    if @comment.user != current_user
+      flash[:alert] = "Not authorized to edit comment."
+      redirect_to request.referrer
+    end
   end
 
   def update
+    @photo = Photo.find(params[:photo_id])
+    @comment = Comment.find(params[:id])
+    if @comment.user != current_user
+      flash[:alert] = "Not authorized to edit this comment."
+    else
+      @comment.update(comment_params)
+    end
+    redirect_to user_photo_path(@photo.user, @photo)
   end
 
   def destroy
@@ -29,7 +44,7 @@ class CommentsController < ApplicationController
       @comment.destroy
       flash[:notice] = "Comment successfully deleted."
     end
-    redirect_to user_photo_path(current_user, @photo)
+    redirect_to request.referrer
   end
 
   private
