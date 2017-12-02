088cf1524c69f958cb837db12c77c92fd700c7a6
Dan McQuade <dtmcquade@gmail.com>
2017-12-02 16:45:28 -0500

    Update styles for favorites feature

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -1,6 +1,8 @@
-$(document).on('turbolinks:load', function() {
-  $("#favicon").click(function() {
+$(document).on('turbolinks:load', function () {
+  $('#favicon').click(function () {
     $(this).toggleClass('no-fav')
+    $(this).find('i').toggleClass('fa-star-o', 'fa-star')
     $(this).toggleClass('fav')
+    $(this).find('i').toggleClass('fa-star', 'fa-star-o')
   })
 })
